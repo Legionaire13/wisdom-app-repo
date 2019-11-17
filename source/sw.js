2301c41fbb717663ab6882d4e3e46741baa90fc5
legionaire13 <legionaire13@mail.ru>
2019-11-17 15:56:19 +0300

    feat: sw.js

---
+++ source/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('wisdom-app').then(function(cache) {
+     return cache.addAll([
+       './',
+      //  './index.html',
+      //  './index.html?homescreen=1',
+      //  './?homescreen=1',
+      //  './js/app.min.js',
+      //  './css/style.min.css',
+      //  './img'
+     ]);
+   })
+ );
+});
+
+// Intercept the web page requests
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+ });
