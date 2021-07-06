cd10b9b88adab3b522d652eae5792b303762ad4a
legionaire13 <legionaire13@mail.ru>
2021-07-06 20:20:23 +0300

    refactor: sw.js

--- source/sw.js
+++ source/sw.js
@@ -1,26 +1,25 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('wisdom-app').then(function(cache) {
-     return cache.addAll([
-       './',
-      //  './index.html',
-      //  './index.html?homescreen=1',
-      //  './?homescreen=1',
-      //  './js/app.min.js',
-      //  './css/style.min.css',
-      //  './img'
-     ]);
-   })
- );
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("wisdom-app").then(function (cache) {
+      return cache.addAll([
+        //  './',
+        //  './index.html',
+        //  './js/app.min.js',
+        //  './css/style.min.css',
+        "favicon.png",
+        "./img",
+      ]);
+    })
+  );
 });
 
 // Intercept the web page requests
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   // console.log(event.request.url);
 
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
- });
+});
